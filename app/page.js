35de86a368688c72ef34dffb6f3513240bb31aff
Ajiboye Ismail <ishmaeldotun@gmail.com>
2025-10-31 19:43:33 +0000

    responsive now

--- app/page.js
+++ app/page.js
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-[#151515] text-white h-[100vh] flex justify-between px-[5%] pt-20 pb-5">
-      <main className="w-[45%]">
+    <div className="bg-[#151515] text-white h-fit lg:h-[100vh] block lg:flex justify-between px-[5%] pt-14 md:pt-20 pb-5">
+      <main className="w-[100%] md:w-[80%] lg:w-[50%] xl::w-[45%]">
         <div>
-          <h1 className="text-5xl font-bold">Ismail Ajiboye.</h1>
-          <h1 className="mt-3 text-xl font-bold">
+          <h1 className="text-4xl md:text-5xl font-bold">Ismail Ajiboye.</h1>
+          <h1 className="mt-1 sm:mt-3 text-xl font-bold">
             Frontend And React Engineer.
           </h1>
-          <p className="text-gray-400 mt-10">
+          <p className="text-gray-400 mt-5 md:mt-10">
             I create interactive{" "}
             <span className="text-white font-bold">Web Experiences</span> using
             frontend technologies that solve real world problems.
@@ -65,97 +65,87 @@ export default function Home() {
           </Link>
         </nav>
       </main>
-      <aside className="w-[45%] overflow-auto  no-scrollbar">
+      <aside className="w-[100%] sm:w-[80%] lg:w-[45%] overflow-auto  mt-10 lg:mt-0 no-scrollbar">
         <p className="text-gray-500">Projects</p>
         <figure className="mt-5">
           <div className="bg-[#1c1c1f] p-8">
             <h1 className="text-lg">Quick Cart</h1>
-            <p className="text-[0.8rem] mt-2 text-gray-400">
-              NEXT JS + TAILWIND
-            </p>
+            <p className="text-[0.78rem]  text-gray-400">NEXT JS + TAILWIND</p>
             <p className="mt-5 mb-7 text-gray-600 text-[0.78rem]">
               An E-commerce website for a gadget company with view of products.
             </p>
             <Link
               href="https://deequickcart.vercel.app/"
-              className=" text-gray-300 text-[0.9rem]"
+              className=" text-gray-300 text-[0.9rem] underline"
             >
               Live site
             </Link>
           </div>
           <div className="bg-[#1c1c1f] p-8 mt-7">
             <h1 className="text-lg">Burgershop</h1>
-            <p className="text-[0.8rem] mt-2 text-gray-400">
-              NEXT JS + TAILWIND
-            </p>
+            <p className="text-[0.78rem] text-gray-400">NEXT JS + TAILWIND</p>
             <p className="mt-5 mb-7 text-gray-600 text-[0.78rem]">
               Landing page for a Burgershop where users can check for burgers.
             </p>
             <Link
               href="https://deeburgershop.vercel.app/"
-              className=" text-gray-300 text-[0.9rem]"
+              className=" text-gray-300 text-[0.9rem] underline"
             >
               Live site
             </Link>
           </div>
           <div className="bg-[#1c1c1f] p-8 mt-7">
             <h1 className="text-lg">Invoice Creator</h1>
-            <p className="text-[0.8rem] mt-2 text-gray-400">
-              NEXT JS + TAILWIND
-            </p>
+            <p className="text-[0.78rem] text-gray-400">NEXT JS + TAILWIND</p>
             <p className="mt-5 mb-7 text-gray-600 text-[0.78rem]">
               An Invoice issuing website that can be used by buisness owners to
               track buisness transaction.
             </p>
             <Link
               href="https://deeinvoice-app.vercel.app/"
-              className=" text-gray-300 text-[0.9rem]"
+              className=" text-gray-300 text-[0.9rem] underline"
             >
               Live site
             </Link>
           </div>
           <div className="bg-[#1c1c1f] p-8 mt-7">
             <h1 className="text-lg">Ip Address Tracker</h1>
-            <p className="text-[0.8rem] mt-1 text-gray-400">REACT + TAILWIND</p>
+            <p className="text-[0.78rem] text-gray-400">REACT + TAILWIND</p>
             <p className="mt-5 mb-7 text-gray-600 text-[0.78rem]">
               A webpage that tracks and display Ip address,location and time of
               a user.
             </p>
             <Link
               href="https://deeiptracker.netlify.app/"
-              className=" text-gray-300 text-[0.9rem]"
+              className=" text-gray-300 text-[0.9rem] underline"
             >
               Live site
             </Link>
           </div>
           <div className="bg-[#1c1c1f] p-8 mt-7">
             <h1 className="text-lg">Country Search</h1>
-            <p className="text-[0.8rem] mt-1 text-gray-400">
-              NEXT JS + TAILWIND
-            </p>
+            <p className="text-[0.78rem] text-gray-400">NEXT JS + TAILWIND</p>
             <p className="mt-5 mb-7 text-gray-600 text-[0.78rem]">
               A webpage where user can learn about all countries,
               Region,Population,Capital,Currency etc
             </p>
             <Link
               href="https://dee-country.vercel.app/"
-              className=" text-gray-300 text-[0.9rem]"
+              className=" text-gray-300 text-[0.9rem] underline"
             >
               Live site
             </Link>
           </div>
           <div className="bg-[#1c1c1f] p-8 mt-7">
             <h1 className="text-lg">Packing List</h1>
-            <p className="text-[0.8rem] mt-1 text-gray-400">
-              NEXT JS + TAILWIND
-            </p>
+            <p className="text-[0.78rem] text-gray-400">NEXT JS + TAILWIND</p>
             <p className="mt-5 mb-7 text-gray-600 text-[0.78rem]">
               A webpage that a user can use to list important things to be
               done,more like a Todo
             </p>
             <Link
               href="https://dee-packinglist.vercel.app/"
-              className=" text-gray-300 text-[0.9rem]"
+              className=" text-gray-300 text-[0.9rem] underline"
             >
               Live site
             </Link>
